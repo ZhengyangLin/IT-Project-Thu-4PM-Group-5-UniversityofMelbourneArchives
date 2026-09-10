9b66b3c8cb669dc98545f9abbecdb0aafd8c90aa
guanchengl10524 <guanchengl1@student.unimelb.edu.au>
2026-09-10 16:34:17 +1000

    Add tests for drawing file
    Added tests to check existence and non-emptiness of drawing files.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,40 @@
-def test_ci_is_working():
-    assert True
+from pathlib import Path
+import pytest
+
+
+
+TEST_DIR = Path(__file__).parent
+
+
+FIXTURES_DIR = TEST_DIR / "fixtrues"
+
+
+@pytest.mark.parametrize(
+    "image_name",
+    [
+        "drawing1.jpg",
+        "drawing2.jpg",
+    ]
+)
+def test_drawing_exists(image_name):
+    """Test that the drawing exists."""
+
+    image_path = FIXTURES_DIR / image_name
+
+    assert image_path.exists(), f"Image not found: {image_path}"
+
+
+@pytest.mark.parametrize(
+    "image_name",
+    [
+        "drawing1.jpg",
+        "drawing2.jpg",
+    ]
+)
+def test_drawing_not_empty(image_name):
+    """Test that the drawing file is not empty."""
+
+    image_path = FIXTURES_DIR / image_name
+
+    assert image_path.exists(), f"Image not found: {image_path}"
+    assert image_path.stat().st_size > 0, f"Image is empty: {image_path}"
